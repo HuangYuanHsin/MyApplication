37684b2e8545e7d20a5ebabefe23adb0ac844f32
Hsin <m1586m1586@gmail.com>
2018-01-04 23:03:15 +0800

    13233

--- app/src/main/java/com/example/m1586/myapplication/MainActivity.java
+++ app/src/main/java/com/example/m1586/myapplication/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else {
 
-                    errorMessage.setText("");
+                    //errorMessage.setText("");
 
                 }
 
